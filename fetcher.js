ae8e2ac95604aac0c4afc8e928977d1d8b8585c5
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-12 13:10:06 -0400

    fetcher

---
+++ fetcher.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const request = require('request');
+const arg = process.argv.slice(2)
+const content = 'Some content!';
+
+const makingFile = function () {
+  request.get(arg[0], (error, response, body) => {
+    if (error) {
+      return error;
+    }
+    fs.writeFile(arg[1], body, (err) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(`Downloded and saved ${body.length} bytes to ${arg[1]}`);
+
+    });
+  })
+
+};
+
+makingFile();
+
